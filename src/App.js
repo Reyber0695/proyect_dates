d5b8295fac6940f070b1654c9a59723366745bbc
Reyber Santos <dragonite0695@gmail.com>
2021-03-25 14:26:41 -0600

    proyecto node especificado 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState, useEffect } from 'react';
-import Formulario from './components/Formulario'
+import Formulario from './components/Formulario';
 import Cita from './components/Cita';
 
 
